3a0c031588ba23b1d668575aabb7d4ebbeda919e
Gilad Novik <gilad@novik.ca>
2019-06-08 15:33:54 +0300

    add AES encryption for password digest

--- models/user.js
+++ models/user.js
@@ -2,28 +2,25 @@
 const crypto = require('crypto');
 const util = require('util');
 
-const SCRYPT_SALT_SIZE = 16;
-const SCRYPT_KEYLEN = 64;
+const SCRYPT_SALT_SIZE = 10;
+const SCRYPT_ROUNDS = 8;
+const SCRYPT_MEM_COST = 14;
 
 const randomBytes = util.promisify(crypto.randomBytes);
 const scrypt = util.promisify(crypto.scrypt);
 
-async function scryptHash(password) {
-    const salt = await randomBytes(SCRYPT_SALT_SIZE);
-    const hash = await scrypt(password, salt, SCRYPT_KEYLEN);
-    const res = Buffer.alloc(hash.length + salt.length + 4);
-    res.writeUInt32BE(salt.length, 0, true);
-    salt.copy(res, 4);
-    hash.copy(res, salt.length + 4);
-    return res.toString('base64');
-}
-
-async function scryptVerify(password, passwordHash) {
-    const buf = Buffer.from(passwordHash, 'base64');
-    const saltLength = buf.readUInt32BE(0);
-    const hash = await scrypt(password, buf.slice(4, saltLength + 4), buf.length - saltLength - 4);
-    return !hash.compare(buf, saltLength + 4);
-}
+const calculateDigest = async function(password, salt) {
+    const cipher = crypto.createCipheriv(
+        'aes-256-ctr',
+        await scrypt(password, Buffer.from(salt, 'base64'), 32, {
+            N: 1 << SCRYPT_MEM_COST,
+            p: 1,
+            r: SCRYPT_ROUNDS,
+        }),
+        Buffer.alloc(16, 0)
+    );
+    return cipher.update(process.env.SIGNER_KEY || 'c2VjcmV0', 'base64', 'base64') + cipher.final('base64');
+};
 
 module.exports = (sequelize, Sequelize) => {
     const model = sequelize.define(
@@ -32,20 +29,28 @@ module.exports = (sequelize, Sequelize) => {
             email: {type: Sequelize.STRING, unique: true},
             password: Sequelize.VIRTUAL,
             password_digest: Sequelize.STRING,
+            salt: Sequelize.STRING,
             logout: Sequelize.DATE,
         },
         {
             hooks: {
                 async beforeCreate(user) {
-                    if (user.password) user.set('password_digest', await scryptHash(user.password));
+                    if (user.password) {
+                        user.salt = (await randomBytes(SCRYPT_SALT_SIZE)).toString('base64');
+                        user.set('password_digest', await calculateDigest(user.password, user.salt));
+                    }
                 },
                 async beforeUpdate(user) {
-                    if (user.password) user.set('password_digest', await scryptHash(user.password));
+                    if (user.password) {
+                        user.salt = (await randomBytes(SCRYPT_SALT_SIZE)).toString('base64');
+                        user.set('password_digest', await calculateDigest(user.password, user.salt));
+                    }
                 },
             },
         }
     );
+
     model.prototype.authenticate = async function(password) {
-        return this.password_digest && (await scryptVerify(password, this.password_digest));
+        return this.password_digest && this.password_digest === (await calculateDigest(String(password), this.salt));
     };
 };
